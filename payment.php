380566cc1751505943255df0265c82b8b1eaef51
sahilsamant01 <92201776+sahilsamant01@users.noreply.github.com>
2023-11-25 23:43:39 +0530

    Add files via upload

---
+++ payment.php
@@ -0,0 +1,27 @@
+<?php
+
+
+require_once('config.php');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Letstravel</title>
+<body>
+  <h1>Booking Successful! </h1>
+</body>
+
+<div class="background-pic"></div>
+<style>
+h1 {text-align: center;}
+body {
+  background-image: url("https://images.pexels.com/photos/1078981/pexels-photo-1078981.jpeg?cs=srgb&dl=pexels-oliver-sj%C3%B6str%C3%B6m-1078981.jpg&fm=jpg");
+}
+body,text{
+    padding: 350px;
+}
+</style>
+</html>
+
+
+
